88978a2409f895fdc3957c928b9264634e1364e4
ItaloMedici <italomedice969@gmail.com>
2021-05-06 08:46:02 -0300

    configure lib

--- main.c
+++ main.c
@@ -3,53 +3,8 @@
 #include<string.h>
 #include<time.h>
 
-void openFile(char *linkFile, int sizeFile, char *string){
-  FILE *file;
-
-  // Abre o arquivo passado no parâmetro e seleciona a forma de abertura, nesse
-  // caso o r seria para a leiura.
-  file = fopen(linkFile, "r");
-
-  if(file == NULL){
-
-    // Mapeia o erro obtido e imprime a mensagem de erro na saida
-    perror("Erro no arquivo");
-    exit(0);
-  }
-
-  memset(string, '\0', sizeFile);
-  
-  char text[sizeFile];
-
-  // Faz a leitura do arquivo até que nulo
-  while(fgets(text, sizeFile, file) != NULL){
-    //concatena as duas strings
-    strcat(string, text);
-  }
-
-  fclose(file);
-}
-
-int sizeFile(char *linkFile){
-  int length;
-
-  FILE *file = fopen(linkFile, "r");
-
-  if(file == NULL){
-    perror("Erro no arquivo");
-    exit(0);
-  }
-
-  // reposiciona o indicador de posição do fluxo (arquivo)  
-  // em função do deslocamento, nesse caso para o final (SEEK_END).
-  fseek(file, 0, SEEK_END);
-
-  // retorna o valor do indicador de posução do fluxo (arquivo)
-  length = ftell(file);
-  fclose(file);
-
-  return length;
-}
+#include "libs/openFile.h"
+#include "libs/sizeFile.h"
 
 int main (int argc, char *argv[] ){
 
