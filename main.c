02d8a24e81bc7a6ed5c7fd8b3741db7c74a366c5
ItaloMedici <italomedice969@gmail.com>
2021-05-09 02:52:44 -0300

    update main.c

--- main.c
+++ main.c
@@ -3,24 +3,42 @@
 #include<string.h>
 #include<time.h>
 
-#include "libs/openFile.h"
-#include "libs/sizeFile.h"
+// Libs customizadas
+#include "libs/utils.h"
 
 int main (int argc, char *argv[] ){
+  int typeResearch = 0;
+  int sizeOfFile;
 
+  // Verifica se o 2 argumento é falso
   if(argv[2] == NULL) {
     printf("Insira um arquivo para pesquisa");
     return 0;
   }
-
-  char teste2;
-
-  int sizeOfFile = sizeFile(argv[2]);
-  char textString[sizeOfFile];
-
-  openFile("text.txt", sizeOfFile, textString);
   
-  puts(textString);
+  // Recebe qual o tipo de pesquisa passada pelo usuário
+  typeResearch = getTypeResearch(argv[1]);
+
+  // Entra se o tipo de pesquisa for positivo
+  if(typeResearch >= 0){
+
+    // Pega o tamanho do arquivo passado
+    sizeOfFile = sizeFile(argv[2]);
+    
+    // Cria um vetor com o tamanho do arquivo
+    char textString[sizeOfFile];
+    
+    // Abre o arquivo e copia para o vetor textString
+    openFile("text.txt", sizeOfFile, textString);
+
+    puts(textString);
+
+    printf("Contagem: %i", wordsCounter(textString));
+    
+
+  } else {
+    printf("Pesquisa invalida");
+  }
 
   system("pause");
   return 0;
