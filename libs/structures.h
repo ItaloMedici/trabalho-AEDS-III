eb7e42dbb91106c34a9c375554bc92083bfccc5f
ItaloMedici <italomedice969@gmail.com>
2021-05-12 14:16:02 -0300

    add new linked struct on stringStruct

--- libs/structures.h
+++ libs/structures.h
@@ -2,6 +2,7 @@
 #define MAX_LETTERS 1024
 
 typedef struct stringStruct {
-    char *word[21];
-    int usedCounter;
+    char word[21];
+    int usageCounter;
+    struct stringStruct *next;
 }stringStruct;
