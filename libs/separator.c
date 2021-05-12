eefad01c0a07eb3a0902784400250422f306a196
ItaloMedici <italomedice969@gmail.com>
2021-05-12 14:14:38 -0300

    create new sepator lib

---
+++ libs/separator.c
@@ -0,0 +1,15 @@
+#include "separator.h"
+
+int wordsInjected = 0;
+int totalWords = 0;
+
+int splitter(char *string, int typeOfResearch){
+
+    // Total geral de palavras 
+    totalWords = wordsCounter(string);
+
+    char *split = NULL;
+    char *nextWord = NULL;
+
+}
+
